2b40ceaa43b011305ab3d6887a8386e8aef9a370
Bombel <bombel@bombel.com>
2020-01-01 12:57:33 +0100

    Add clicked all href

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
 
@@ -13,13 +14,23 @@ function titleClickHandler(event){
   }
 
   /* [IN PROGRESS] add class 'active' to the clicked link */
-  
+
   clickedElement.classList.add('active');
   console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
 
+  const activeArticles = document.querySelectorAll('.posts .active');
+
+  for(let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
+
   /* get 'href' attribute from the clicked link */
+  
+  const articleSelector = clickedElement.getAttribute('href');
+
+  console.log('Link was clicked', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
