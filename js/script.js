ef435f308ee0d2610797d98e4e8f8b5d6a620ac5
Bombel <bombel@bombel.com>
2020-01-01 12:25:43 +0100

    Add class active

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
-const activeLinks = document.querySelectorAll('.titles a.active');
+  /* [DONE] remove class 'active' from all article links  */
 
-for (let activeLink of activeLinks) {
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
-}
-  /* add class 'active' to the clicked link */
+  }
 
-  /* remove class 'active' from all articles */
-const activeArticles = document.querySelectorAll('.posts .active');
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-for (let activeArticle of activeArticles) {
-    activeArticle.classList.remove('active');
-}
+  /* [DONE] remove class 'active' from all articles */
+
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
@@ -30,3 +31,4 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
