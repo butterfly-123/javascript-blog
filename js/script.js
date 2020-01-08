579b9e42247357641db65fb97770569ec56285af
Bombel <bombel@bombel.com>
2020-01-08 09:39:17 +0100

    Install packet eslint and make correction in js

--- js/script.js
+++ js/script.js
@@ -11,37 +11,29 @@ function generateTitleLinks(){
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
 
-  function clearMessages(){
-    document.getElementById('messages').innerHTML = '';
-  }
-
   /* for each article */
 
   const articles = document.querySelectorAll(optArticleSelector);
 
   let html = '';
 
-    for (let article of articles) {
+  for (let article of articles) {
+    /* get the article id */
+    const articleId = article.getAttribute('id');
 
-      /* get the article id */
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-      const articleId = article.getAttribute('id');
+    /* get the title from the title element */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+    
+    /* insert link into titleList */
+    html = html + linkHTML;
+  }
 
-      /* find the title element */
-
-      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-
-      /* get the title from the title element */
-
-      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-      console.log(linkHTML);
-
-      
-      /* insert link into titleList */
-      html = html + linkHTML;
-    }
-    console.log(html);
-    titleList.innerHTML = html;
+  console.log(html);
+  titleList.innerHTML = html;
 }
 
 generateTitleLinks();
@@ -52,59 +44,59 @@ generateTitleLinks();
 // Funkcja, która nadaje zdarzenie poprzez kliknięcie tytułu
 function titleClickHandler(event){
   // ?
-event.preventDefault();
+  event.preventDefault();
   // ?
-let clickedElement = this;
+  let clickedElement = this;
   // Sprawdzenie, czy funkcja kliknięcie tytułu działa
-console.log('Link was clicked!');
+  console.log('Link was clicked!');
 
-/* [DONE] remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
-// Zmienna, której zostają przypisane aktywne linki
-const activeLinks = document.querySelectorAll('.titles a.active');
+  // Zmienna, której zostają przypisane aktywne linki
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-// Pętla, która przechodzi przez kazdy link i sprawdza, który jest aktywny
-for(let activeLink of activeLinks){
+  // Pętla, która przechodzi przez kazdy link i sprawdza, który jest aktywny
+  for(let activeLink of activeLinks){
     // Jezeli link jest aktywny to usówa ta klasę
-  activeLink.classList.remove('active');
-}
+    activeLink.classList.remove('active');
+  }
 
-/* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-// Przez kliknięcie linka nadaję aktywną klasę
-clickedElement.classList.add('active');
-// Wyświetlenie linku, któremu została nadana aktywna klasa poprzez kliknięcie
-console.log('clickedElement:', clickedElement);
+  // Przez kliknięcie linka nadaję aktywną klasę
+  clickedElement.classList.add('active');
+  // Wyświetlenie linku, któremu została nadana aktywna klasa poprzez kliknięcie
+  console.log('clickedElement:', clickedElement);
 
-/* [DONE] remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
 
-// Zmienna, której zostają przypisane aktywne artykuły
-const activeArticles = document.querySelectorAll('.posts .active');
+  // Zmienna, której zostają przypisane aktywne artykuły
+  const activeArticles = document.querySelectorAll('.posts .active');
 
-// Pętla, która przechodzi przez kazdy artykuł i sprawdza, który jest aktywny
-for(let activeArticle of activeArticles) {
+  // Pętla, która przechodzi przez kazdy artykuł i sprawdza, który jest aktywny
+  for(let activeArticle of activeArticles) {
     // Jezeli artykuł jest aktywny to usówa ta klasę
-  activeArticle.classList.remove('active');
-}
+    activeArticle.classList.remove('active');
+  }
 
-/* get 'href' attribute from the clicked link */
+  /* get 'href' attribute from the clicked link */
 
-// Zmienna, której zostaje przypisany kazdy artykuł po kliknięciu 
-const articleSelector = clickedElement.getAttribute('href');
-// Pokazanie klikniętego linka
-console.log('Link was clicked', articleSelector);
+  // Zmienna, której zostaje przypisany kazdy artykuł po kliknięciu 
+  const articleSelector = clickedElement.getAttribute('href');
+  // Pokazanie klikniętego linka
+  console.log('Link was clicked', articleSelector);
 
-/* find the correct article using the selector (value of 'href' attribute) */
+  /* find the correct article using the selector (value of 'href' attribute) */
 
-// ?
-const targetArticle = document.querySelector(articleSelector);
-// Wyswietla, który link został klikniety
-console.log('Clicked href', targetArticle);
+  // ?
+  const targetArticle = document.querySelector(articleSelector);
+  // Wyswietla, który link został klikniety
+  console.log('Clicked href', targetArticle);
 
-/* add class 'active' to the correct article */
+  /* add class 'active' to the correct article */
 
-// Nadanie dodatkowej actywnej klasy 
-targetArticle.classList.add('active');
+  // Nadanie dodatkowej actywnej klasy 
+  targetArticle.classList.add('active');
 }
 
 // Zmienna, której zostaja przypisany wszystkie linki
@@ -113,5 +105,5 @@ const links = document.querySelectorAll('.titles a');
 // Pętla, która przechodzi przez kazdy link
 for(let link of links){
   // Do kazdego linki nadaje nasluchiwacz klik i po kliknięciu wywołuje funkcje mozliwości kliknięcia linku
-link.addEventListener('click', titleClickHandler);
+  link.addEventListener('click', titleClickHandler);
 }
